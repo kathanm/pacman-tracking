768183dd5ccc8eab48c8fa782c95df05dbf4e650
Kathan Master <kathan.master@yahoo.com>
2019-03-15 01:06:23 -0400

    Implemented observe function

--- tracking/inference.py
+++ tracking/inference.py
@@ -148,19 +148,23 @@ class ExactInference(InferenceModule):
         emissionModel = busters.getObservationDistribution(noisyDistance)
         pacmanPosition = gameState.getPacmanPosition()
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
         # Replace this code with a correct observation update
         # Be sure to handle the "jail" edge case where the ghost is eaten
         # and noisyDistance is None
+        if noisyDistance is None:
+            allPossible = util.Counter()
+            for p in self.legalPositions:
+                allPossible[p] = 0
+            allPossible[self.getJailPosition()] = 1
+            self.beliefs = allPossible
+            return
+
         allPossible = util.Counter()
         for p in self.legalPositions:
             trueDistance = util.manhattanDistance(p, pacmanPosition)
             if emissionModel[trueDistance] > 0:
-                allPossible[p] = 1.0
+                allPossible[p] = self.beliefs[p] * emissionModel[trueDistance]
 
-        "*** END YOUR CODE HERE ***"
 
         allPossible.normalize()
         self.beliefs = allPossible
